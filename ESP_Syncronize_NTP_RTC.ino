8832615e0094445d79098c861d822369cf14f7f3
Thorik123 <tronikthorik07@gmail.com>
2024-06-09 22:32:11 +0700

    #

--- ESP_Syncronize_NTP_RTC.ino
+++ ESP_Syncronize_NTP_RTC.ino
@@ -10,8 +10,8 @@
 #define DS1307
 #define DS3231
 
-const char *ssid = "Polsek Batealit";
-const char *password = "thoriktronik";
+const char *ssid = "xxxxxxxxxxxxxxxx";
+const char *password = "xxxxxxxxxxxx";
 
 #ifdef DS1307
 RTC_DS1307 rtc;
@@ -23,57 +23,59 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "id.pool.ntp.org");
 
 char daysOfTheWeek[7][12] = {
-  "Minggu",
-  "Senin",
-  "Selasa",
-  "Rabu",
-  "Kamis",
-  "Jum'at",
-  "Sabtu"
-};
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jum'at",
+    "Sabtu"};
 
 String weekDays[7] = {
-  "Minggu",
-  "Senin",
-  "Selasa",
-  "Rabu",
-  "Kamis",
-  "Jum'at",
-  "Sabtu"
-};
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jum'at",
+    "Sabtu"};
 
 String months[12] = {
-  "Januari",
-  "Februari",
-  "Maret",
-  "April",
-  "Mei",
-  "Juni",
-  "Juli",
-  "Agustus",
-  "September",
-  "Oktober",
-  "November",
-  "Desember"
-};
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"};
 
 unsigned long lastTime;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
-  if (!rtc.begin()) {
+  if (!rtc.begin())
+  {
     Serial.println("Couldn't find RTC");
     Serial.flush();
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
 #ifdef DS1307
-  if (!rtc.isrunning()) {
+  if (!rtc.isrunning())
+  {
     Serial.println("RTC is NOT running, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 #elif DS3231
-  if (!rtc.lostPower()) {
+  if (!rtc.lostPower())
+  {
     Serial.println("RTC is NOT running, let's set the time!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
@@ -82,7 +84,8 @@ void setup() {
   Serial.print("Connecting WiFi to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -95,27 +98,33 @@ void setup() {
   time_t epochTime = timeClient.getEpochTime();
   struct tm *ptm = gmtime((time_t *)&epochTime);
 
-  if (timeClient.isTimeSet()) {
+  if (timeClient.isTimeSet())
+  {
     rtc.adjust(DateTime(ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday, timeClient.getHours(), timeClient.getMinutes(), timeClient.getSeconds()));
     Serial.print(F("Setting time success\n"));
     WiFi.disconnect();
     WiFi.mode(WIFI_OFF);
-  } else {
+  }
+  else
+  {
     Serial.print(F("Setting time failed\n"));
     WiFi.disconnect();
     WiFi.mode(WIFI_OFF);
   }
 }
 
-void loop() {
-  if (millis() - lastTime >= 1000) {
+void loop()
+{
+  if (millis() - lastTime >= 1000)
+  {
     showRTCTime();
     showNTPTime();
     lastTime = millis();
   }
 }
 
-void showRTCTime() {
+void showRTCTime()
+{
   DateTime now = rtc.now();
   Serial.println("RTC Time");
   Serial.println(String("Formatted Time: ") + now.timestamp(DateTime::TIMESTAMP_TIME));
@@ -123,7 +132,8 @@ void showRTCTime() {
   Serial.println();
 }
 
-void showNTPTime() {
+void showNTPTime()
+{
   timeClient.update();
   Serial.println("NTP Time");
   time_t epochTime = timeClient.getEpochTime();
